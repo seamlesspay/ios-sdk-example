25bf45afb241e96def3d5a525d0b7ad8bc292372
seamless-pay-ios <seamlesspayios@gmail.com>
2024-06-16 16:36:35 +0100

    Initial commit

---
+++ Example/Views/ContentView.swift
@@ -0,0 +1,66 @@
+// **
+// * Copyright (c) Seamless Payments, Inc.
+// *
+// * This source code is licensed under the MIT license found in the
+// * LICENSE file in the root directory of this source tree.
+// *
+
+import SwiftUI
+import SeamlessPay
+
+enum DemoAuth {
+  static let secretKey: String = "sk_XXXXXXXXXXXXXXXXXXXXXXXXXX"
+  static let environment: SeamlessPay.Environment = .staging
+  static let proxyAccountId: String? = "MRT_XXXXXXXXXXXXXXXXXXXXXXXXXX"
+}
+
+struct ContentView: View {
+  @State private var cardFormContentType: CardFormContentType?
+
+  var body: some View {
+    NavigationView {
+      List {
+        Section {
+          Text("Singleline Card Form")
+            .onTapGesture {
+              self.cardFormContentType = .single
+            }
+          Text("Multiline Card Form")
+            .onTapGesture {
+              self.cardFormContentType = .multi
+            }
+        } header: {
+          Text("UI Components")
+        }
+      }
+      .sheet(item: $cardFormContentType) { contentType in
+        NavigationStack {
+          CardFormContent(
+            authorization: .init(
+              secretKey: DemoAuth.secretKey,
+              proxyAccountId: DemoAuth.proxyAccountId,
+              environment: DemoAuth.environment
+            ),
+            type: contentType
+          )
+          .navigationBarItems(
+            trailing: Button("Done") {
+              self.cardFormContentType = .none
+            }
+          )
+        }
+      }
+      .navigationTitle("SeamlessPay UI Demo")
+    }
+  }
+}
+
+#Preview {
+  ContentView()
+}
+
+extension SeamlessPay.Environment: Identifiable {
+  public var id: Int {
+    rawValue
+  }
+}
---
+++ README.md
@@ -0,0 +1,46 @@
+# SeamlessPay iOS Example Application
+
+## Overview
+
+The iOS Example Application demonstrates the basic usage of UI components. The application includes examples of initializing UI components with different configurations. It enables you to process tokenization, refunds, and charge requests.
+
+## Configuration
+
+Make sure to replace the placeholder authorization data with your own to successfully execute the requests.
+
+```swift
+enum DemoAuth {
+  static let secretKey: String = "sk_XXXXXXXXXXXXXXXXXXXXXXXXXX"
+  static let environment: SeamlessPay.Environment = .staging
+  static let proxyAccountId: String? = "MRT_XXXXXXXXXXXXXXXXXXXXXXXXXX"
+}
+
+```
+
+You can also modify the field configuration yourself and initialize UI components with this data.
+
+```swift
+init(authorization: Authorization, type: CardFormContentType) {
+    let fieldOptions = FieldOptions.default
+
+    switch type {
+    case .single:
+      cardForm = SingleLineCardForm(
+        authorization: authorization,
+        fieldOptions: fieldOptions
+      )
+    case .multi:
+      cardForm = MultiLineCardForm(
+        authorization: authorization,
+        fieldOptions: .init(
+          cvv: .init(display: .required),
+          postalCode: .init(display: .required)
+        )
+      )
+    }
+  }
+```
+
+## Build and Run
+
+Once you have completed the setup, select the "Example" scheme, and run the project.
