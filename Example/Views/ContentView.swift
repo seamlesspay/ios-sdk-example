ca05fc579027e1b41489c3322ad71cc65aebb03c
seamless-pay-ios <134730126+seamless-pay-ios@users.noreply.github.com>
2025-04-21 14:13:57 +0100

    feat: [EXI-23 EXI-18] Update the Home screen for the iOS example app; As a user, I want to toggle between Light and Dark modes (#17)

--- Example/Views/ContentView.swift
+++ Example/Views/ContentView.swift
@@ -15,61 +15,100 @@ enum DemoAuth {
 }
 
 struct ContentView: View {
+  enum AppStorageKeys {
+    static let isDarkMode = "isDarkMode"
+  }
+  
+  @AppStorage(AppStorageKeys.isDarkMode) private var isDarkMode = {
+    if UserDefaults.standard.object(forKey: AppStorageKeys.isDarkMode) == nil {
+      return UITraitCollection.current.userInterfaceStyle == .dark
+    }
+    return UserDefaults.standard.bool(forKey: AppStorageKeys.isDarkMode)
+  }()
+
   @State private var contentType: ContentType?
 
   var body: some View {
     NavigationView {
       List {
         Section {
+          Toggle("Dark mode", isOn: $isDarkMode)
+        }
+        
+        Section {
           Button("Card Form") {
             self.contentType = .cardForm
           }
+          .tint(.primary)
+          
           Button {
             self.contentType = .applePay
           } label: {
             Text("Apple Pay Button")
               .withApplePaySimulatorNotice()
           }
+          
         } header: {
           Text("UI Components")
         }
+        .tint(.primary)
       }
-      .sheet(item: $contentType) { contentType in
+      .sheet(item: $contentType) { type in
         NavigationStack {
-          Group {
-            switch contentType {
-            case .cardForm:
-              CardFormContent(
-                header: "Card Form",
-                config: .init(
-                  environment: DemoAuth.environment,
-                  secretKey: DemoAuth.secretKey,
-                  proxyAccountId: DemoAuth.proxyAccountId
-                ),
-                fieldOptions: FieldOptions(
-                  cvv: FieldConfiguration(display: .required),
-                  postalCode: FieldConfiguration(display: .required)
-                ),
-                styleOptions: .default
-              )
-            case .applePay:
-              ApplePayContent(
-                config: .init(
-                  environment: DemoAuth.environment,
-                  secretKey: DemoAuth.secretKey,
-                  proxyAccountId: DemoAuth.proxyAccountId
-                )
-              )
-            }
-          }
-          .navigationBarItems(
-            trailing: Button("Done") {
-              self.contentType = .none
-            }
-          )
+          sheetContent(for: type)
+            .navigationBarItems(
+              trailing: Button("Done") {
+                self.contentType = .none
+              }
+            )
         }
       }
-      .navigationTitle("SeamlessPay UI Demo")
+      .navigationTitle("SDK Examples")
+      .onChange(of: isDarkMode) {
+        updateAppearance()
+      }
+      .onAppear {
+        updateAppearance()
+      }
+    }
+  }
+  
+  @ViewBuilder
+  private func sheetContent(for contentType: ContentType) -> some View {
+    switch contentType {
+    case .cardForm:
+      CardFormContent(
+        header: "Card Form",
+        config: .init(
+          environment: DemoAuth.environment,
+          secretKey: DemoAuth.secretKey,
+          proxyAccountId: DemoAuth.proxyAccountId
+        ),
+        fieldOptions: FieldOptions(
+          cvv: FieldConfiguration(display: .required),
+          postalCode: FieldConfiguration(display: .required)
+        ),
+        styleOptions: .default
+      )
+    case .applePay:
+      ApplePayContent(
+        config: .init(
+          environment: DemoAuth.environment,
+          secretKey: DemoAuth.secretKey,
+          proxyAccountId: DemoAuth.proxyAccountId
+        )
+      )
+    }
+  }
+}
+
+// MARK: Helper
+private extension ContentView {
+  func updateAppearance() {
+    if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+      windowScene.windows.forEach { window in
+        window.overrideUserInterfaceStyle = isDarkMode ? .dark : .light
+      }
     }
   }
 }
