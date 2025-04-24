55946288fb7fdcedde058e40b7675776de4ce050
seamless-pay-ios <134730126+seamless-pay-ios@users.noreply.github.com>
2025-04-24 15:49:17 +0100

    feat: [exi-26]: Update the Apple Pay Button Screen (#20)
    * feat: [exi-26] Update the Apple Pay Button Screen
    
    * feat: [EXI-26] Introduce Transaction model and integrate into ApplePayContent for payment processing

--- Example/Views/ApplePayContent.swift
+++ Example/Views/ApplePayContent.swift
@@ -5,16 +5,17 @@
 // * LICENSE file in the root directory of this source tree.
 // *
 
-import SwiftUI
 import PassKit
 import SeamlessPay
+import SwiftUI
 
 struct ApplePayContent: View {
-  @State var status: RequestStatus = .idle
-
+  @State private var result: PaymentResponseResult?
+  @State private var isRquestInProgress: Bool = false
   @State var applePayHandler: ApplePayHandler?
-
   private let config: ClientConfiguration
+
+  @State private var transaction: Transaction = .charge(amount: "")
 
   init(config: ClientConfiguration) {
     self.config = config
@@ -24,58 +25,91 @@ struct ApplePayContent: View {
     VStack(spacing: 16) {
       if let applePayHandler {
         if applePayHandler.canPerformPayments {
-          ApplePayButtonUI {
-            withAnimation {
-              status = .processing
-            }
-
-            applePayHandler.presentApplePayFor(ChargeRequest(amount: 125)) { result in
-              withAnimation {
-                switch result {
-                case let .success(response):
-                  status = .success(response.debugDescription)
-                case let .failure(error):
-                  status = .failure(error.debugDescription)
-                case .canceled:
-                  status = .failure("Payment was canceled by the user.")
-                }
-              }
-            }
+          ZStack {
+            form
+            paymentProgressView
           }
-          .frame(width: 200)
-          .frame(height: 50)
-          .withApplePaySimulatorNotice()
         } else {
-          Text(
-            "Payment processing is not available. The `ApplePayHandler.canPerformPayments` is `false`. Please check SeamlessPay SDK configuration. And your device's capabilities to satisfy the `PKPaymentAuthorizationController.canMakePayments()`"
-          )
+          unavailablePaymentText
         }
       } else {
         ProgressView()
       }
-
-      Group {
-        HStack {
-          if let iconName = status.iconName {
-            Image(systemName: iconName)
-          }
-          Text(status.header)
-            .lineLimit(1)
-            .fontWeight(.bold)
-        }
-        if status.inProgress {
-          ProgressView()
-        } else {
-          Text(status.payload)
-            .multilineTextAlignment(.leading)
-            .lineLimit(.max)
-        }
-      }
-      .foregroundColor(status.color)
     }
-    .padding()
+    .overlay {
+      if isRquestInProgress {
+        Color.black.opacity(0.25)
+          .ignoresSafeArea()
+      }
+    }
+    .navigationTitle("Card Form")
+    .navigationBarTitleDisplayMode(.inline)
+    .navigationDestination(item: $result) { value in
+      PaymentResponseView(
+        result: value
+      )
+    }
     .task {
       self.applePayHandler = await ApplePayHandler(config: config)
+    }
+  }
+
+  private var form: some View {
+    Form {
+      Section {
+        TextField(
+          "Amount $",
+          text: Binding(
+            get: { transaction.amount },
+            set: { transaction = .charge(amount: $0) }
+          )
+        )
+        .keyboardType(.decimalPad)
+      }
+    }
+    .safeAreaInset(edge: .bottom) {
+      ApplePayButtonUI {
+        withAnimation {
+          isRquestInProgress = true
+        }
+
+        applePayHandler?.presentApplePayFor(
+          ChargeRequest(amount: transaction.cents)
+        ) { result in
+          withAnimation {
+            isRquestInProgress = false
+            switch result {
+            case let .success(response):
+              self.result = .init(kind: .success, value: response.debugDescription)
+            case let .failure(error):
+              self.result = .init(kind: .failure, value: error.localizedDescription)
+            case .canceled:
+              self.result = .none
+            }
+          }
+        }
+      }
+      .frame(width: 200)
+      .frame(height: 50)
+      .withApplePaySimulatorNotice()
+      .disabled(transaction.amount.isEmpty)
+      .padding()
+    }
+  }
+
+  private var unavailablePaymentText: some View {
+    Text(
+      """
+      Payment processing is not available. 
+      The `ApplePayHandler.canPerformPayments` is `false`. Please check SeamlessPay SDK configuration. And your device's capabilities to satisfy the `PKPaymentAuthorizationController.canMakePayments()`
+      """
+    )
+  }
+
+  @ViewBuilder
+  var paymentProgressView: some View {
+    if isRquestInProgress {
+      ProgressView()
     }
   }
 }
